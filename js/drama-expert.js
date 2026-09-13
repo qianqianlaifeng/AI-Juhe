d262b418da57894dc93a5bcca20f88d7d5fe89cf
zlx-cn2 <2337835909@qq.com>
2026-09-13 19:36:52 +0800

    fix: remove comment causing syntax error in AGNES_CONFIG

--- js/drama-expert.js
+++ js/drama-expert.js
@@ -11,7 +11,7 @@
   'use strict';
 
   const AGNES_CONFIG = {
-    baseURL: 'https://api.agnes-ai.cn/v1'  // API endpoint,
+    baseURL: 'https://api.agnes-ai.cn/v1',
     apiKey: 'sk-5dxkoayGKuy09DeveyAnlYUHRUzlE6xx9j4RUKHDqcNHoFZ8',
     textModel: 'agnes-2.5-flash',
     imageModel: 'agnes-image-2.5-flash'
